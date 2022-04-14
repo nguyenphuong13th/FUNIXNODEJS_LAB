3cd266e8fdbd0e7f792bc991b8f4beadc828d666
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-04-14 23:10:05 +0700

    show some html element by node js

--- second-app.js
+++ second-app.js
@@ -2,7 +2,12 @@ const http = require('http');//importing http module
 //creating server
 const server = http.createServer((req,res)=>
 {
-    console.log(req);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My Second App</title></head>');
+    res.write('<body><h1>Hello from my Node.js server</h1></body>');
+    res.write('</html>');
+    res.end();
 })
 //listening to port
 server.listen(3000);// listrning to port 3000 loccalhost:3000
