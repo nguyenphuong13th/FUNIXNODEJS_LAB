11fab58dbeca795540b2864b8f643cb02fe55ea2
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-04-18 01:15:25 +0700

    autostart using nodemon package

--- routes.js
+++ routes.js
@@ -45,6 +45,7 @@ const handler = (req, res) => {
   res.write("<body><h1>Hello from my Node.js server</h1></body>");
   res.write("</html>");
   res.end();
+
 };
 //method 1
 // export.handler = handler;
