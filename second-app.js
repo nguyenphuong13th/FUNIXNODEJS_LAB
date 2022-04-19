de00202838866b16cc2a6dcb6845353f8f177d76
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-04-19 23:09:57 +0700

    install Middleware and execute 2 simple midleware

--- second-app.js
+++ second-app.js
@@ -1,6 +1,14 @@
 const http = require('http');//importing http module
 const express = require('express');//importing express module
 const app = express();//creating an express application
+app.use((req, res, next) => {
+    console.log('this is the first middleware');
+    next();//allows the request to continue to the next middleware
+})//1st middleware
+app.use((req, res, next) => {
+    console.log('this is the second middleware');
+})//1st middleware
+
 const server = http.createServer(app);//creating a server
 //listening to port
 server.listen(3000);// listrning to port 3000 loccalhost:3000
