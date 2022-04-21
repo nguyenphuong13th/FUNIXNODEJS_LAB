901bf26bd862f3827bee3e8a28eaf00d0c151e47
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-04-21 23:57:58 +0700

    handle multi route

--- second-app.js
+++ second-app.js
@@ -4,12 +4,14 @@ const app = express();//creating an express application
 app.use((req, res, next) => {
     console.log('this is the first middleware');
     next();//allows the request to continue to the next middleware
-})//1st middleware
-app.use((req, res, next) => {
-    console.log('this is the second middleware');
-    res.send('<h1>Hello from Express.js</h1>');//sending a response
+})//1st middleware always runs
+app.use('/add-product',(req, res, next) => {
+    res.send('<h1>The Add product page</h1>');//sending a response
 })//2nd middleware
-
-const server = http.createServer(app);//creating a server
-//listening to port
-server.listen(3000);// listrning to port 3000 loccalhost:3000
+app.use('/',(req, res, next) => {
+    res.send('<h1>Hello from Express.js</h1>');//sending a response
+})//3rd middleware
+// const server = http.createServer(app);//creating a server
+// //listening to port
+// server.listen(3000);// listrning to port 3000 loccalhost:3000
+app.listen(3000);//listrning to port 3000 loccalhost:3000
