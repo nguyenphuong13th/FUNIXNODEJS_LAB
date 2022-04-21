c0ac95b086f6181b8058beaa5264548214f02b1d
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-04-22 00:41:18 +0700

    analyz posting method from form

--- second-app.js
+++ second-app.js
@@ -1,16 +1,18 @@
 const http = require('http');//importing http module
 const express = require('express');//importing express module
 const app = express();//creating an express application
-app.use((req, res, next) => {
-    console.log('this is the first middleware');
-    next();//allows the request to continue to the next middleware
-})//1st middleware always runs
+const bodyParser = require('body-parser');//importing body-parser module
+app.use(bodyParser.urlencoded({extended:false}));//using body-parser
 app.use('/add-product',(req, res, next) => {
-    res.send('<h1>The Add product page</h1>');//sending a response
+    res.send('<html><body><form action = "/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></body></html>');//sending a response
 })//2nd middleware
 app.use('/',(req, res, next) => {
     res.send('<h1>Hello from Express.js</h1>');//sending a response
 })//3rd middleware
+app.use('/product',(req, res, next) => {
+    console.log(req.body);//logging the request body
+    res.redirect('/');//back to the root
+})//4th middleware
 // const server = http.createServer(app);//creating a server
 // //listening to port
 // server.listen(3000);// listrning to port 3000 loccalhost:3000
