6d3c49b8dbc6f93fe5d0a47af171b77a177d91f7
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-04-22 23:35:11 +0700

    Create 404 page not found page

--- second-app.js
+++ second-app.js
@@ -6,6 +6,9 @@ const shopRoutes = require('./routes/shop');//importing shop routes
 app.use(bodyParser.urlencoded({extended:true}));//using body-parser
 app.use(adminRoutes);//using admin routes
 app.use(shopRoutes);//using shop routes
+app.use ((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');//sending a response
+});
 // const server = http.createServer(app);//creating a server
 // //listening to port
 // server.listen(3000);// listrning to port 3000 loccalhost:3000
