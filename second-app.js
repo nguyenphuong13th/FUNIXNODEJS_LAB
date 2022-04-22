7935e191a5f28c35d87815c17d75dac6a4c495d3
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-04-22 23:02:40 +0700

    change use method to post method

--- second-app.js
+++ second-app.js
@@ -1,16 +1,16 @@
 const express = require('express');//importing express module
 const app = express();//creating an express application
 const bodyParser = require('body-parser');//importing body-parser module
-app.use(bodyParser.urlencoded({extended:false}));//using body-parser
+app.use(bodyParser.urlencoded({extended:true}));//using body-parser, to parse the body of the request which can contain anytype of data
 app.use('/add-product',(req, res, next) => {
     res.send('<html><body><form action = "/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></body></html>');//sending a response
 })//2nd middleware
-app.use('/',(req, res, next) => {
+app.get('/',(req, res, next) => {// use get method if use use post method then it will not run into post method
     res.send('<h1>Hello from Express.js</h1>');//sending a response
 })//3rd middleware
 app.post('/product',(req, res, next) => { // only for post requests
     console.log(req.body);//logging the request body
-    res.redirect('/');//back to the root
+    res.redirect("/");//back to the root
 })//4th middleware
 // const server = http.createServer(app);//creating a server
 // //listening to port
