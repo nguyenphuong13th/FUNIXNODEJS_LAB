42f05e1dce6b4ce898967f48eb5a1f0848a7d2ba
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-04-22 23:56:59 +0700

    Adding two new html pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,11 @@
 const express=require('express');
 
 const router=express.Router();
+//admin/add-product => GET
 router.get('/add-product',(req, res, next) => {
     res.send('<html><body><form action = "/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></body></html>');//sending a response, action is the url where the form will be submitted, method is the method used to submit the form (POST or GET)
 })
+//admin/add-product => POST
 router.post('/add-product',(req, res, next) => { // only for post requests url can be the same with get
     console.log(req.body);//logging the request body
     res.redirect("/");//back to the root
