76afcd2a24784a7bb4f4d616042e7a1e1d5994d6
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-05-27 00:22:24 +0700

    storing data in file via model

--- models/product.js
+++ models/product.js
@@ -1,11 +1,22 @@
-const products = [];
+const fs = require('fs');// working with file systems
+const path = require('path');// importing path module to work with file
 
 module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
     save() {
-        products.push(this);
+        const p = path.join(path.dirname(require.main.filename),
+        'data',
+        'products.json');// getting the path of the file which store the products.json
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);// should use arrow function or this will be a conext and will not refer to class anymore
+            fs.writeFile(p, JSON.stringify(products), (err) => {console.log(err)})// writing the products.json file
+        });
     }
     static fetchAll() {
         return products;
