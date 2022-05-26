a28a6ec59004f5c97f2fd66d1048b7e18d8a06be
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-05-26 23:45:03 +0700

    finish lab 4.2

--- controller/404.js
+++ controller/404.js
@@ -2,5 +2,5 @@ const path = require('path');//importing path module
 exports.get404Error = (req, res, next) => {
     // res.status(404).sendFile(path.join(__dirname,'views','404-Error.html'));//sending a response
     res.status(404).render('404-Error',
-    {pageTitle:'404'});//rendering the 404.pug file in views folder automatically
+    {pageTitle:'page not found', path: path});//rendering the 404.pug file in views folder automatically
 }
