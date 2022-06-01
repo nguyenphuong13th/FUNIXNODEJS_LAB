3329d583f0560d9a7415f740957593f7d27f247e
PhuongNguyen <phuongnguyenhoang13th@gmail.com>
2022-06-01 23:30:51 +0700

    refactoring the file storage code

--- models/product.js
+++ models/product.js
@@ -1,33 +1,41 @@
 const fs = require('fs');// working with file systems
 const path = require('path');// importing path module to work with file
+const p = path.join(path.dirname(require.main.filename),
+        'data',
+        'products.json'
+        );// getting the path of the file which store the products.json
 
+const getProductFromFile = cb =>{
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                cb([]);
+            }else{
+                cb(JSON.parse(fileContent))
+            };
+        });
+
+}
 module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
     save() {
-        const p = path.join(path.dirname(require.main.filename),
-        'data',
-        'products.json');// getting the path of the file which store the products.json
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductFromFile(products => {
             products.push(this);// should use arrow function or this will be a conext and will not refer to class anymore
             fs.writeFile(p, JSON.stringify(products), (err) => {console.log(err)})// writing the products.json file
         });
+
     }
     static fetchAll(cb) {
-        const p = path.join(path.dirname(require.main.filename),
-        'data',
-        'products.json');// getting the path of the file which store the products.json
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
-
+        // const p = path.join(path.dirname(require.main.filename),
+        // 'data',
+        // 'products.json');// getting the path of the file which store the products.json
+        // fs.readFile(p, (err, fileContent) => {
+        //     if (err) {
+        //         cb([]);
+        //     }
+        //     cb(JSON.parse(fileContent));
+        // });
+        getProductFromFile(cb);
     }
 }
